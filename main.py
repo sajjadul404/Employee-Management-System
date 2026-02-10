c1988b9ccd772f755b39afd7d81bb69b05bb4819
sajjadul404 <sajjadul625@gmail.com>
2026-02-10 23:44:07 +0600

    Emploee-Management-System

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-<<<<<<< HEAD
 print ("Employee-Management-System")
 print ("Sajjadul Islam")
 print ("01560060092")
-=======
 print ("Employee-Management-System")
 print ("Sajjadul Islam")
->>>>>>> 7ec45e134507c6f22a77b425caadf8d7ec24dcc6
