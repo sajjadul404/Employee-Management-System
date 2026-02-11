84fe9e7ce596e6f57327560c7bb2bee6b67da52f
sajjadul404 <sajjadul625@gmail.com>
2026-02-11 16:51:21 +0600

    Fram 1

--- main.py
+++ main.py
@@ -6,8 +6,11 @@ class employee:
         self.root.title("Employee Management System")
         self.root.geometry("1400x650+0+0")
         self.mainLabel = tk.Label(self.root,bd=5,relief="groove", text="Employee Management System", font=("Arial", 40, "bold"), bg="black", fg="gold")
-        self.mainLabel.pack(side=tk.TOP, fill=tk.X)
+        self.mainLabel.pack(side=tk.TOP, fill=tk.X) 
 
+        #......Fram1......
+        self.Fram1 = tk.Frame(self.root, bd=5, relief="groove", width=350, height=550)
+        self.Fram1.place(x=10, y=80)
 root = tk.Tk()
 obj = employee(root)
 root.mainloop()
