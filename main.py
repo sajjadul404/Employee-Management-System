c51d58d5a629bc7974d4df2e7d403706c29addfa
sajjadul404 <sajjadul625@gmail.com>
2026-02-11 12:30:15 +0600

    mainLabel.pack

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ class employee:
     def __init__(self, root):
         self.root = root
         self.root.title("Employee Management System")
+        self.root.geometry("1400x650+0+0")
+        self.mainLabel = tk.Label(self.root,bd=5,relief="groove", text="Employee Management System", font=("Arial", 40, "bold"), bg="black", fg="gold")
+        self.mainLabel.pack(side=tk.TOP, fill=tk.X)
 
 root = tk.Tk()
 obj = employee(root)
