aa1bdcde929c3aa2e927f1eee4e883f20f5e6b0e
sajjadul404 <sajjadul625@gmail.com>
2026-02-12 00:34:27 +0600

    self.idIn

--- main.py
+++ main.py
@@ -10,11 +10,13 @@ class employee:
 
 
         #......Fram1......
-        self.Fram1 = tk.Frame(self.root, bd=5, relief="groove")
+        self.Fram1 = tk.Frame(self.root, bd=5, relief="groove",bg="lightblue")
         self.Fram1.place(x=10, y=80,width=350, height=550)
 
-        self.idLable = tk.Label(self.Fram1, padx=5, text="Employee ID",bg="lightblue",fg="white", font=("Arial", 14, "bold"),pady=10)
+        self.idLable = tk.Label(self.Fram1, padx=5, text="Employee ID:",fg="Black",bg="lightblue", font=("Arial", 14, "bold"),pady=10)
         self.idLable.grid(row=0, column=0)
+        self.idIn = tk.Entry(self.Fram1, font=("Arial", 14), width=15)
+        self.idIn.grid(row=0, column=1,pady=10)
 
 
         #......Fram2......
